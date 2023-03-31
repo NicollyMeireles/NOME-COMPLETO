810b471cbd02bb13058da109e6dd1ad7313b4d30
NicollyMeireles <nicollymeireles32@gmail.com>
2023-03-31 20:29:16 -0300

    NOME-COMPLETO

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿string nome, sobrenome;
+Console.WriteLine ("digite seu nome _____________");
+nome = Console.ReadLine()!;
+
+Console.Write("digite seu sobrenome________________");
+sobrenome = Console.ReadLine()!;
+
+Console.WriteLine($"nome completo: {nome} {sobrenome}");
+Console.WriteLine($"nome de catálogo: {sobrenome.ToUpper()}, {nome}");
